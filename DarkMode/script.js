4697039425f25800566d58f1f9c59db07d17e358
Yoon Me Me <yoonmeme.engr@gmail.com>
2024-11-14 17:26:29 +0630

    first commit

---
+++ DarkMode/script.js
@@ -0,0 +1,27 @@
+// const changeMode = document.getElementById("#changeMode");
+
+// const changeToDark = () => {
+//     document.documentElement.setAttribute("theme", "dark");
+//     localStorage.setItem("theme", "dark");
+// };
+
+// const changeToLight = () => {
+//     document.documentElement.setAttribute("theme", "light");
+//     localStorage.setItem("theme", "light");
+// };
+
+// changeMode.addEventListener("change", () => {
+//     const changeTheme = localStorage.getItem("theme");
+//     if(changeTheme === "dark") {
+//         changeToLight();
+//     } else {
+//         changeToDark();
+//     }
+// });
+
+// const changeTheme = localStorage.getItem("theme");
+// if(changeTheme === "dark") {
+//     changeToDark();
+// } else {
+//     changeToLight();
+// }
